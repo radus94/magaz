cfae1d520d20462baef42d532feff7f2b7af912d
RaduSirghe <radusirghev@gmail.com>
2020-12-08 23:45:26 +0200

    commit

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,70 @@
 <template>
   <div class="search">
-    <input type="text" />
+    <input type="text" v-model="search"/>
     <button>search</button>
     <router-link class="add-btn" to="/add-auto">ADD</router-link>
+    <div class="search-result" v-if="carsData.length || truckData.length">
+      <h5>Result:</h5>
+      <div class="search-in-car" v-if="carsData.length">
+        Autoturisme: {{carsData.length}}
+      </div>
+      <div class="search-in-car" v-if="truckData.length">
+        Camioane: {{truckData.length}}
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      search: '',
+      carsData: [],
+      truckData: []
+    }
+  },
+  computed: {
+    ...mapGetters(['CARS', 'TRUCK']),
+    fullName: () => (marc, model) => {
+      console.log()
+      if (marc.length <= 3) {
+        return marc.toUpperCase() + " " + model;
+      } else {
+        return marc + " " + model;
+      }
+    }
+  },
+  methods: {
+    getIfExist(array, search) {
+      return array.filter(item => {
+        if (search != '') {
+        if(search.toLowerCase() === this.fullName(item.Marc, item.Model).toLowerCase() || 
+        this.fullName(item.Marc, item.Model).toLowerCase().includes(search.toLowerCase())) {
+          return item
+        }
+        }
+      })
+    },
+    getSearchedData(type, search) {
+      if(type === 'autoturisme'){
+        this.carsData = this.getIfExist(this.CARS, search)
+      } else if (type === 'camioane') {
+        this.truckData = this.getIfExist(this.TRUCK, search)
+        console.log(this.truckData)
+      }
+    }
+  },
+  watch: {
+    search: function (newVal) {
+      this.getSearchedData('autoturisme',newVal)
+      this.getSearchedData('camioane',newVal)
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 .search {
